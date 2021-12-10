5dfc8fec563091257332dc73de83a3d750fe38c1
QTFIUE <95908233+QTFIUE@users.noreply.github.com>
2021-12-10 18:38:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,9 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# push packages to package/lean
+
+pushd package/lean
+
+#add openwrt-packages kenzok8源
+git clone --depth=1 https://github.com/kenzok8/openwrt-packages
